b7836c8b47a1c39ab3062de75414bdb554f3c024
Xavi Serrano <xserrano2001@gmail.com>
2025-02-17 10:02:24 +0100

    8o proyecto drag&drop

--- 8. Tarjetas/js/script.js
+++ 8. Tarjetas/js/script.js
@@ -10,5 +10,73 @@ const $mainContent = document.querySelector('.content');
 // }, 2000);
 
 
-const $importImage = document.querySelector('.importImage');
+/* CREATE OWN CARD */
+
+/* Lógica de importar la imagen */
+const $importZone = document.querySelector('.import-zone');
+const $importInput = document.querySelector(".import-image");
+const $customizedCard = document.querySelector(".customized-card");
+const $cardModal = document.querySelector(".card-modal");
+
+$importZone.addEventListener("click", function() {
+  $importInput.click(); // Simula un clic en el input file
+  $customizedCard.classList.remove("show"); 
+});
+
+$importInput.addEventListener("change", function(event) {
+  handleFile(event.target.files[0]);
+  updateDownloadLink();
+});
+
+/* Lógica de arrastrar la imagen */
+
+// Permitir arrastrar sobre import-zone sin que el navegador lo bloquee
+$importZone.addEventListener("dragover", (event) => {
+  event.preventDefault();
+  $importZone.style.border = "2px dashed black";
+  $importZone.style.backgroundColor = "#fefefe";
+});
+
+// Restaurar estilo cuando el mouse sale del área
+$importZone.addEventListener("dragleave", () => {
+  $importZone.style.border = "2px dashed gray";
+  $importZone.style.backgroundColor = "#cecece";
+  updateDownloadLink();
+});
+
+// Manejar el archivo cuando se suelta sobre import-zone
+$importZone.addEventListener("drop", (event) => {
+  event.preventDefault();
+  $importZone.style.border = "2px dashed gray";
+  $importZone.style.backgroundColor = "#cecece";
+  const file = event.dataTransfer.files[0]; // Obtener el archivo
+  if (file) {
+    handleFile(file);
+  }
+});
+
+function handleFile(file) {
+  if (file && file.type.startsWith("image/")) {
+    const imageURL = URL.createObjectURL(file);
+    $customizedCard.src = imageURL;
+    $cardModal.style.display = "block";
+
+    setTimeout(() => {
+      $customizedCard.classList.add("show"); 
+    }, 10);
+  } else {
+    console.log("Por favor, sube un archivo de imagen válido.");
+  }
+}
+
+
+/* Download Button */
+const $downloadButton = document.querySelector('.download-button');
+
+function updateDownloadLink() {
+  const imageSrc = $customizedCard.src;
+  $downloadButton.href = imageSrc;
+  updateDownloadLink();
+}
+
 
